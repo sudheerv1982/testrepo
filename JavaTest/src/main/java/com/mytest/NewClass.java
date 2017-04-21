fcd38cfd386b3f9418eca537d4e297307bfd1cc9
Sudheer Vedanaparthi <Sudheer Vedanaparthi@192.168.0.108>
2017-04-20 19:15:22 -0500

    Test Commit

---
+++ JavaTest/src/main/java/com/mytest/NewClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mytest;
+
+/**
+ * @author Sudheer Vedanaparthi
+ *
+ */
+public class NewClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
